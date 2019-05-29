003fd65f3f2155e880710dc60898089d71b93513
Patrick <Patricknnoonan@gmail.com>
2019-05-29 15:52:04 -0500

    Ran Some sql queries to improve table data

--- HumaneDBseedsBrett..sql
+++ HumaneDBseedsBrett..sql
@@ -1,75 +1,75 @@
 
-INSERT INTO Clients (FirstName, LastName, UserName, Password, Email)
-VALUES ('Alexander', 'Harris', 'xander', 'password', 'xander@sunnydale.com')
+--INSERT INTO Clients (FirstName, LastName, UserName, Password, Email)
+--VALUES ('Alexander', 'Harris', 'xander', 'password', 'xander@sunnydale.com')
 
-INSERT INTO Clients (FirstName, LastName, UserName, Password, Email)
-VALUES ('Willow', 'Rosenberg', 'willow', 'password', 'willow@sunnydale.com');
+--INSERT INTO Clients (FirstName, LastName, UserName, Password, Email)
+--VALUES ('Willow', 'Rosenberg', 'willow', 'password', 'willow@sunnydale.com');
 
-INSERT INTO Clients (FirstName, LastName, UserName, Password, Email)
-VALUES ('Daniel', 'Osbourne', 'oz', 'password', 'oz@sunnydale.com');
+--INSERT INTO Clients (FirstName, LastName, UserName, Password, Email)
+--VALUES ('Daniel', 'Osbourne', 'oz', 'password', 'oz@sunnydale.com');
 
-INSERT INTO Clients (FirstName, LastName, UserName, Password, Email)
-VALUES ('Rupert', 'Giles', 'giles', 'password', 'giles@sunnydale.com');
+--INSERT INTO Clients (FirstName, LastName, UserName, Password, Email)
+--VALUES ('Rupert', 'Giles', 'giles', 'password', 'giles@sunnydale.com');
 
-INSERT INTO Clients (FirstName, LastName, UserName, Password, Email)
-VALUES ('Faith', 'Lehane', 'faith', 'passowrd', 'faith@sunnydale.com');
+--INSERT INTO Clients (FirstName, LastName, UserName, Password, Email)
+--VALUES ('Faith', 'Lehane', 'faith', 'passowrd', 'faith@sunnydale.com');
 
-INSERT INTO Employees(FirstName, LastName, UserName, Password, EmployeeNumber, Email)
-VALUES ('Jessica', 'Shepherd', 'shepherd', 'password', 01, 'shepherd@normandy.com');
+--INSERT INTO Employees(FirstName, LastName, UserName, Password, EmployeeNumber, Email)
+--VALUES ('Jessica', 'Shepherd', 'shepherd', 'password', 01, 'shepherd@normandy.com');
 
-INSERT INTO Employees
-VALUES ('Garrus', 'Vakarian', 'garrus', 'password', 02, 'garrus@normandy.com');
+--INSERT INTO Employees
+--VALUES ('Garrus', 'Vakarian', 'garrus', 'password', 02, 'garrus@normandy.com');
 
-INSERT INTO Employees
-VALUES ('Tali', 'Rayya', 'tali', 'password', 03, 'tali@normandy.com');
+--INSERT INTO Employees
+--VALUES ('Tali', 'Rayya', 'tali', 'password', 03, 'tali@normandy.com');
 
-INSERT INTO Employees
-VALUES ('Jack', 'Zero', 'jack', 'password', 04, 'jack@normandy.com');
+--INSERT INTO Employees
+--VALUES ('Jack', 'Zero', 'jack', 'password', 04, 'jack@normandy.com');
 
-INSERT INTO Employees
-VALUES ('Urdnot', 'Wrex', 'wrex', 'password', 05, 'wrex@normandy.com');
+--INSERT INTO Employees
+--VALUES ('Urdnot', 'Wrex', 'wrex', 'password', 05, 'wrex@normandy.com');
 
-INSERT INTO Rooms (RoomNumber)
-VALUES (01);
+--INSERT INTO Rooms (RoomNumber)
+--VALUES (01);
 
-INSERT INTO Rooms (RoomNumber)
-VALUES (02);
+--INSERT INTO Rooms (RoomNumber)
+--VALUES (02);
 
-INSERT INTO Rooms (RoomNumber)
-VALUES (03);
+--INSERT INTO Rooms (RoomNumber)
+--VALUES (03);
 
-INSERT INTO Rooms (RoomNumber)
-VALUES (04);
+--INSERT INTO Rooms (RoomNumber)
+--VALUES (04);
 
-INSERT INTO Rooms (RoomNumber)
-VALUES (05);
+--INSERT INTO Rooms (RoomNumber)
+--VALUES (05);
 
-INSERT INTO Rooms (RoomNumber)
-VALUES (06);
+--INSERT INTO Rooms (RoomNumber)
+--VALUES (06);
 
-INSERT INTO Rooms (RoomNumber)
-VALUES (07);
+--INSERT INTO Rooms (RoomNumber)
+--VALUES (07);
 
-INSERT INTO Rooms (RoomNumber)
-VALUES (08);
+--INSERT INTO Rooms (RoomNumber)
+--VALUES (08);
 
-INSERT INTO Rooms (RoomNumber)
-VALUES (09);
+--INSERT INTO Rooms (RoomNumber)
+--VALUES (09);
 
-INSERT INTO Rooms (RoomNumber)
-VALUES (10);
+--INSERT INTO Rooms (RoomNumber)
+--VALUES (10);
 
-INSERT INTO Animals (Name, Weight, Age, Demeanor, KidFriendly, PetFriendly, Gender, AdoptionStatus, EmployeeId)
-VALUES ('Ghost', 200, 5, 'Quiet', 1, 1, 'Male', 'Adopted', 1);
+--INSERT INTO Animals (Name, Weight, Age, Demeanor, KidFriendly, PetFriendly, Gender, AdoptionStatus, EmployeeId)
+--VALUES ('Ghost', 200, 5, 'Quiet', 1, 1, 'Male', 'Adopted', 1);
 
-INSERT INTO Animals (Name, Weight, Age, Demeanor, KidFriendly, PetFriendly, Gender, AdoptionStatus, EmployeeId)
-VALUES ('ShaggyDog', 170, 5, 'Shaggy', 1, 1, 'Male', 'Adopted', 2);
+--INSERT INTO Animals (Name, Weight, Age, Demeanor, KidFriendly, PetFriendly, Gender, AdoptionStatus, EmployeeId)
+--VALUES ('ShaggyDog', 170, 5, 'Shaggy', 1, 1, 'Male', 'Adopted', 2);
 
-INSERT INTO Animals (Name, Weight, Age, Demeanor, KidFriendly, PetFriendly, Gender, AdoptionStatus, EmployeeId)
-VALUES ('Grey Wind', 215, 5, 'Fierce', 0, 0, 'Male', 'Adopted', 3);
+--INSERT INTO Animals (Name, Weight, Age, Demeanor, KidFriendly, PetFriendly, Gender, AdoptionStatus, EmployeeId)
+--VALUES ('Grey Wind', 215, 5, 'Fierce', 0, 0, 'Male', 'Adopted', 3);
 
-INSERT INTO Animals (Name, Weight, Age, Demeanor, KidFriendly, PetFriendly, Gender, AdoptionStatus, EmployeeId)
-VALUES ('Lady', 120, 5, 'Protective', 1, 0, 'Female', 'Adopted', 4);
+--INSERT INTO Animals (Name, Weight, Age, Demeanor, KidFriendly, PetFriendly, Gender, AdoptionStatus, EmployeeId)
+--VALUES ('Lady', 120, 5, 'Protective', 1, 0, 'Female', 'Adopted', 4);
 
-INSERT INTO Animals (Name, Weight, Age, Demeanor, KidFriendly, PetFriendly, Gender, AdoptionStatus, EmployeeId)
-VALUES ('Nymeria', 160, 5, 'Wild', 0, 0, 'Female', 'Adopted', 5);
+--INSERT INTO Animals (Name, Weight, Age, Demeanor, KidFriendly, PetFriendly, Gender, AdoptionStatus, EmployeeId)
+--VALUES ('Nymeria', 160, 5, 'Wild', 0, 0, 'Female', 'Adopted', 5);
